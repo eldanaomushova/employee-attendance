27376dca902a584fd7ea5bcf28bafa0ce8915c4f
EldanaQuuene <eldana.omushova@alatoo.edu.kg>
2023-12-02 16:27:46 +0600

    2com

--- src/main/java/com/example/demo4/HelloController.java
+++ src/main/java/com/example/demo4/HelloController.java
@@ -1,14 +1,45 @@
 package com.example.demo4;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private Label author;
+    @FXML
+    private ImageView image;
+    @FXML
+    private Label name;
+    private List<String> recommendations = new ArrayList<>();
+    Randomblock randomblock = new Randomblock();
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    void onAddTolibrary(ActionEvent event) {
+
+    }
+    public void setDataOfRecommandations(){
+        Image image = new Image(Objects.requireNonNull(getClass().getResourceAsStream(randomblock.getBlockimage())));
+        randomblock.setBlockimage(image);
+
+    }
+
+
+    @FXML
+    void onDeleteFromRecommendation(ActionEvent event) {
+        if (!recommendations.isEmpty()) {
+            String deletedItem = recommendations.remove(0); // Assuming you want to remove the first item
+            System.out.println("Deleted from recommendations: " + deletedItem);
+            // You can update your UI or perform other actions based on the deletion
+        } else {
+            System.out.println("No items in recommendations to delete.");
+        }
+
     }
 }
---
+++ src/main/java/com/example/demo4/Randomblock.java
@@ -0,0 +1,32 @@
+package com.example.demo4;
+
+public class Randomblock {
+    private String blockName;
+    private String blockUsername;
+    private String blockimage;
+
+
+    public String getBlockName() {
+        return blockName;
+    }
+
+    public void setBlockName(String blockName) {
+        this.blockName = blockName;
+    }
+
+    public String getBlockUsername() {
+        return blockUsername;
+    }
+
+    public void setBlockUsername(String blockUsername) {
+        this.blockUsername = blockUsername;
+    }
+
+    public String getBlockimage() {
+        return blockimage;
+    }
+
+    public void setBlockimage(String blockimage) {
+        this.blockimage = blockimage;
+    }
+}
